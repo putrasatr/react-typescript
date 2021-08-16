98081ce912efe802658e5b691a33d5320ea17dce
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-16 14:19:54 +0700

    Components and history create

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { Layout } from "./Layout"
+export { CreatePost } from "./CreatePost"
---
+++ src/history.ts
@@ -0,0 +1,6 @@
+import { createBrowserHistory, History } from "history";
+
+const history: History = createBrowserHistory();
+
+export default history;
+
