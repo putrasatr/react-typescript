e96ae051e53e58f4b094cd57175c0de1ee6fdaa0
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-20 09:40:19 +0700

    modify Layout UI

--- src/components/Layout/navbar.tsx
+++ src/components/Layout/navbar.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+type LinkProps = {
+    urlTo: string;
+    name: string;
+}
+const links: LinkProps[] = [
+    { name: "Home", urlTo: "/" },
+    { name: "Post", urlTo: "/post/23" },
+    { name: "Gallery", urlTo: "/gallery" }
+]
 
 export const Navbar: React.FC = () => {
     return (
         <div className="App-logo">
             <div className="jumbotron">
+                <h1 className="jumbotron__text">Otobai.</h1>
                 <h1 className="jumbotron__text">Ride With Style</h1>
                 <button className="jumbotron__btn__login">Login</button>
             </div>
             <nav>
                 <ul>
-                    <Link className="item__link" to="/">
-                        <li>
-                            <span>Home</span>
-                        </li>
-                    </Link>
-                    <Link className="item__link" to="/post/12">
-                        <li>
-                            <span>Post</span>
-                        </li>
-                    </Link>
-                    <Link className="item__link" to="/gallery">
-                        <li>
-                            <span>Gallery</span>
-                        </li>
-                    </Link>
+                    {links.map(({ urlTo, name }: LinkProps, i: number) => (
+                        <Link className="item__link" to={urlTo} key={i}>
+                            <li>
+                                <span>{name}</span>
+                            </li>
+                        </Link>
+                    ))}
                 </ul>
             </nav>
         </div>
