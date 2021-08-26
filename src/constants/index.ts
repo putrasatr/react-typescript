e70735fcadca471190527f3f4d2294180a63037d
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-26 10:31:01 +0700

    Committin Update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,10 @@ export type Props = {
     children?: any;
 }
 
-export const API_URL = "http://localhost:3002/"
-export const API_URL_GQL = "http://localhost:4001/graphql"
-export const API_URL_OTOBAI = "http://localhost:4001/"
+// export const API_URL = "http://localhost:3002/"
+// export const API_URL_GQL = "http://localhost:4001/graphql"
+// export const API_URL_OTOBAI = "http://localhost:4001/"
+
+export const API_URL = "http://192.168.1.58:3002/"
+export const API_URL_GQL = "http://192.168.1.58:4001/graphql"
+export const API_URL_OTOBAI = "http://192.168.1.58:4001/"
--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -2,7 +2,7 @@ import { Gallery, Home } from "../pages";
 import { CreatePost, DetailItem } from "../components";
 
 export const routers = [
-    { path: "/detail/:image", isExact: true, Component: DetailItem},
+    { path: "/detail/:bikeId", isExact: true, Component: DetailItem},
     { path: "/post/:productId", isExact: true, Component: CreatePost },
     { path: "/gallery", isExact: true, Component: Gallery },
     { path: "/", isExact: true, Component: Home },
