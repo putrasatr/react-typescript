af1e6e01240314544162940e0c16d3442936ed62
putrasatr <satriaputrapratama2002@gmail.com>
2021-08-26 10:27:46 +0700

    content array constant for home

---
+++ src/constants/contents.ts
@@ -0,0 +1,23 @@
+
+export interface ContentProps {
+    title: string;
+    desc: string;
+}
+export const content: ContentProps[] = [
+    {
+        title: "Exploring",
+        desc: "Exploring anything about Motorcycle, Riding, and Adventuring"
+    },
+    {
+        title: "Reviewing",
+        desc: "Reviewing all stuff about biking"
+    },
+    {
+        title: "Communicating",
+        desc: "Communicating with all member and riding lovers"
+    },
+    {
+        title: "Gathering",
+        desc: "If there's one thing I'm good at, it's gathering people together to do something fun"
+    }
+]
