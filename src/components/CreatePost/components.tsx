9cc1a65e955ae2d5566f0bf0cf70463a9cdaac35
putrasatr <satriaputrapratama2002@gmail.com>
2021-09-02 20:50:10 +0700

    CreatePost Modify

--- src/components/CreatePost/components.tsx
+++ src/components/CreatePost/components.tsx
@@ -1,46 +1,71 @@
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { checkPalindrome } from "../../helpers";
-import ItemList from "./itemList"
 
 interface Props {
     id?: string;
     productId: string;
 }
+type DataProps = { [data: string]: string }
+type InputProps = { [text: string]: string }
 
 const Component = ({ match }: RouteComponentProps<Props>) => {
     const { params } = match
-    const [text, setInputText] = React.useState('')
-    const [data, setData] = React.useState<never[] | string[]>([])
+    const [text, setInputText] = React.useState<InputProps>({})
+    let [data, setData] = React.useState<DataProps>({})
     const handleChange = React.useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            setInputText(event.target.value);
+        ({ target: { value, name } }: React.ChangeEvent<HTMLInputElement>) => {
+            data[name] = value
+            text[name] = value
+            setInputText(text);
+            setData(data)
         },
-        [setInputText]
+        [setInputText, data]
     );
-    const handleSubmit = React.useCallback(
-        (event: React.KeyboardEvent<HTMLInputElement>) => {
-            if (event.which === 13) {
-                const text = event.currentTarget.value.trim();
-                setInputText('');
-                setData((prevData: string[]) => [...prevData, checkPalindrome(text)])
-            }
-        },
-        [setInputText]
-    );
+    // const handleSubmit = React.useCallback(
+    //     (event: React.KeyboardEvent<HTMLInputElement>) => {
+    //         if (event.which === 13) {
+    //             const text = event.currentTarget.value.trim();
+    //             setInputText('');
+    //             setData((prevData: string[]) => [...prevData, checkPalindrome(text)])
+    //         }
+    //     },
+    //     [setInputText]
+    // );
     return (
         <div className="form">
             <div className="form__header">
-                <span>Create A Bike</span>
+                <span>Create A Bike {params.id}</span>
             </div>
-            <input
-                value={text}
-                onChange={handleChange}
-                autoFocus
-                onBlur={() => console.log("okay")}
-                onKeyDown={handleSubmit}
-                className="form__input" />
-            <ItemList data={data} params={params} />
+            <div>
+                <label htmlFor="brand">Brand</label>
+                <input
+                    id="brand"
+                    name="brand"
+                    placeholder="Please enter brand name"
+                    value={text.brand}
+                    onChange={handleChange}
+                    autoFocus
+                    onBlur={() => console.log("okay")}
+                    className="form__input" />
+            </div>
+            <div>
+                <label htmlFor="ev">Engine Volume</label>
+                <input
+                    id="ev"
+                    name="engine_volume"
+                    onChange={handleChange}
+                    type="number"
+                    placeholder="Please enter engine volume"
+                    className="form__input" />
+            </div>
+            <div>
+                <label htmlFor="ev">Description</label>
+                <textarea
+                    id="ev"
+                    name="engine_volume"
+                    placeholder="Please enter engine volume"
+                    className="form__input" />
+            </div>
         </div>
     )
 };
