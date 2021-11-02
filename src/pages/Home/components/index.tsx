4d3f6cc4199732e03a4d39036c23cc3c15eafcbe
putrasatr <satriaputrapratama2002@gmail.com>
2021-11-02 18:09:06 +0700

    Clean Code

--- src/pages/Home/components/index.tsx
+++ src/pages/Home/components/index.tsx
@@ -1,27 +1,27 @@
 import React, { Fragment } from "react";
-import Fade from 'react-reveal/Fade'
+import Fade from "react-reveal/Fade";
 
 import { PropsRoute } from "../../../constants";
 import { content, ContentProps } from "../../../constants/contents";
 
 const Home: React.FC<PropsRoute> = (): JSX.Element => {
-    return (
-        <Fragment>
-            <div className="main__wrapper">
-                {content.map(({ title, desc }: ContentProps, i: number): JSX.Element => (
-                    <Fade bottom key={i}>
-                        <div className="content__card">
-                            <strong>{title}</strong>
-                            <div className="content__card__desc">
-                                <span>
-                                    {desc}
-                                </span>
-                            </div>
-                        </div>
-                    </Fade>
-                ))}
-            </div>
-        </Fragment>
-    )
-}
-export default Home
+  return (
+    <Fragment>
+      <div className="main__wrapper">
+        {content.map(
+          ({ title, desc }: ContentProps, i: number): JSX.Element => (
+            <Fade bottom key={i}>
+              <div className="content__card">
+                <strong>{title}</strong>
+                <div className="content__card__desc">
+                  <span>{desc}</span>
+                </div>
+              </div>
+            </Fade>
+          )
+        )}
+      </div>
+    </Fragment>
+  );
+};
+export default Home;
